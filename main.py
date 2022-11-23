a0a6ce376beb6b4d85ccb97dcbcd9c35a98e785f
Ansh <ansh.tiwatne@gmail.com>
2022-11-24 00:15:33 +0530

    fixes

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+
+import pandas as pd
 import pyqrcode
-import png
-from pyqrcode import QRCode
-import csv
 
-url = "https://www.apple.com/"
-qr = pyqrcode.create(url)
+DF = pd.read_csv("images.csv")
 
-qr.png("myqr.png")
+def save_qr():
+    """Generate and save a QR code encoded with a tag"""
+
+    for tag in DF["tag"]:
+        qr = pyqrcode.create(tag)
+        qr.png(f"qrcodes\\{tag}.png")
+
+if __name__ == "__main__":
+    save_qr()
