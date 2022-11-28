e1778f34864b4f04a78e1c999d2cf47a6b30eb04
Ansh <ansh.tiwatne@gmail.com>
2022-11-28 21:03:38 +0530

    created qrcodes folder

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ DF = pd.read_csv("images.csv")
 
 for i in range(len(DF)):
     link = DF["link"][i]
-    space = DF["class"][i]
-    loca = DF["location"][i]
+    area = DF["class"][i]
+    location = DF["location"][i]
     qr = pyqrcode.create(link)
-    qr.png(f"{space}_{loca}.png")
+    qr.png(f".\\qrcodes\\{location}-{area}.png")
--- simplified.py
+++
@@ -1,3 +0,0 @@
-import pandas, pyqrcode
-DF = pandas.read_csv("images.csv")
-[pyqrcode.create(tag).png(f"qrcodes\\{tag}.png") for tag in DF["tag"]]
