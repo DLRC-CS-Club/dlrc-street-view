f8005272246982336319d0cda673c83210a8fc68
Ansh <ansh.tiwatne@gmail.com>
2022-11-30 18:45:30 +0530

    interface created

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 
 import flet as ft
 from flet.buttons import RoundedRectangleBorder
+import pyqrcode
+import pandas as pd
 
 def main(page: ft.Page):
     """flet app"""
@@ -19,13 +21,21 @@ def main(page: ft.Page):
     def generate(e):
         # code goes here
         ...
+        DF = pd.read_csv("images.csv")
+
+        for i in range(len(DF)):
+            link = DF["link"][i]
+            area = DF["class"][i]
+            location = DF["location"][i]
+            qr = pyqrcode.create(link)
+            qr.png(f".\\qrcodes\\{location}-{area}.png")
 
     btn_style = ft.ButtonStyle(
         padding=(page.window_width/3.45), shape=RoundedRectangleBorder(radius=16)
     )
 
     generate_qr = ft.ElevatedButton("Generate QR", icon=ft.icons.FILE_DOWNLOAD, style=btn_style, on_click=generate)
-    pick_files = ft.ElevatedButton("Pick csv files", icon=ft.icons.UPLOAD_FILE, style=btn_style, on_click=lambda _: pick_files_dialog.pick_files(allow_multiple=True))
+    pick_files = ft.ElevatedButton("Pick csv files", icon=ft.icons.UPLOAD_FILE, style=btn_style, on_click=lambda _: pick_files_dialog.pick_files())
     pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
     page.overlay.append(pick_files_dialog)
 
